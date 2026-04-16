4548b6ef5d76fb57529a20ca9be40e3142daa519
Adéla Račanská <racanskaa@email.cz>
2026-04-16 16:37:22 +0200

    ctvrta

--- searching.py
+++ searching.py
@@ -1,8 +1,8 @@
 import os
 import json
-from generators import unordered_sequence, ordered_sequence
 import time
 import matplotlib.pyplot as plt
+from generators import unordered_sequence, ordered_sequence
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -55,6 +55,44 @@ def binary_search(list_of_numbers, the_number):
             max_i = mid_i -1
     return None
 
+def measure_time_of_run():
+    sizes = [100, 500, 1000, 5000, 10000]
+    linear = []
+    binary = []
+
+    for size in sizes:
+        data = unordered_sequence(size)
+        the_number = data[0]
+
+        start = time.perf_counter()
+        linear_search(data,the_number)
+        end = time.perf_counter()
+        result_1 = end - start
+        linear.append(result_1)
+
+        start = time.perf_counter()
+        ordered_sequence = sorted(data)
+        binary_search(ordered_sequence, the_number)
+        end = time.perf_counter()
+        result_2 = end - start
+        binary.append(result_2)
+
+    return sizes, linear, binary
+
+def graph_plot():
+    values = measure_time_of_run()
+    sizes = values[0]
+    linear = values[1]
+    binary = values[2]
+
+    plt.plot(sizes, linear, label="Lineární vyhledávání")
+    plt.plot(sizes, binary, label="Binární vyhledávání")
+
+    plt.xlabel("Velikost vstupu")
+    plt.ylabel("Čas [s]")
+    plt.title("Ukázkový graf měření")
+    plt.show()
+
 
 def main():
     filename = "sequential.json"
@@ -69,6 +107,11 @@ def main():
 
     binary = binary_search(data_ordered, 14)
     print(binary)
+
+    time = measure_time_of_run()
+    print(time)
+
+    graph_plot()
 
 if __name__ == '__main__':
     main()
