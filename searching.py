6d2eac685b0a11e5766b806f5356674d6e134dc1
Adéla Račanská <racanskaa@email.cz>
2026-04-16 15:57:49 +0200

    treti

--- searching.py
+++ searching.py
@@ -1,5 +1,8 @@
 import os
 import json
+from generators import unordered_sequence, ordered_sequence
+import time
+import matplotlib.pyplot as plt
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -23,28 +26,49 @@ def read_data(file_name, field):
         return None
 
 def linear_search(sequence, the_number):
-    positions = []
+    dictionary = {}
     count = 0
-    i = 0
+    position = []
 
-    while i < len(sequence):
-        if sequence[i] == the_number:
-            positions.append(i)
+    for j in range(len(sequence)-1):
+        if sequence[j] == the_number:
             count += 1
-        i += 1
+            position.append(j)
 
-    return {"positions": positions, "count": count}
+    dictionary["positions"] = position
+    dictionary["count"] = count
 
+    return dictionary
 
+def binary_search(list_of_numbers, the_number):
+    min_i = 0
+    max_i = len(list_of_numbers) - 1
+
+    while min_i <= max_i:
+        mid_i = (min_i + max_i) // 2
+
+        if list_of_numbers[mid_i] == the_number:
+            return mid_i
+        elif list_of_numbers[mid_i] < the_number:
+            min_i = mid_i + 1
+        else:
+            max_i = mid_i -1
+    return None
 
 
 def main():
     filename = "sequential.json"
-    data = read_data(filename, "unordered_numbers")
-    print(data)
+    data_unordered = read_data(filename, "unordered_numbers")
+    print(data_unordered)
 
-    searching = linear_search(data, 6)
+    searching = linear_search(data_unordered, 2)
     print(searching)
+
+    data_ordered = read_data(filename, "ordered_numbers")
+    print(data_ordered)
+
+    binary = binary_search(data_ordered, 14)
+    print(binary)
 
 if __name__ == '__main__':
     main()
